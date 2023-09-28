7b3d2c9637fd54b0d5004a60d8e4090eedee4aed
samuel <samu.17.rock@gmail.com>
2023-09-28 02:16:45 -0300

    MEJORANDO VARIAS COSAS

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,60 @@
+// Obtén referencias a los elementos HTML que necesitas
+const serverButton = document.getElementById("server-button");
+const serverList = document.getElementById("server-list");
+
+// Manejador de eventos para el botón "Servidor"
+serverButton.addEventListener("click", () => {
+    // Pregunta al usuario el nombre del servidor (esto puede variar según tus necesidades)
+    const serverName = prompt("Ingrese el nombre del servidor:");
+
+    // Crea un nuevo botón de servidor y agrega al servidor a la lista
+    if (serverName) {
+        const serverButton = document.createElement("button");
+        serverButton.classList.add("server-button");
+        serverButton.textContent = serverName;
+        serverList.appendChild(serverButton);
+
+        // Aquí puedes agregar más lógica, como manejar eventos de clic en los botones de servidor
+        serverButton.addEventListener("click", () => {
+            alert(`Has hecho clic en el servidor: ${serverName}`);
+            // Agrega más lógica aquí según tus necesidades
+        });
+    }
+});
+
+//CANALES
+
+// Obtén referencias a los elementos HTML que necesitas
+const chanelButton = document.getElementById("channel-button");
+const channelList = document.getElementById("channel-list");
+
+// Manejador de eventos para el botón "Servidor"
+chanelButton.addEventListener("click", () => {
+    // Pregunta al usuario el nombre del servidor (esto puede variar según tus necesidades)
+    const channelName = prompt("Ingrese el nombre del servidor:");
+
+    // Crea un nuevo botón de canales y agrega al canal a la lista
+    if (channelName) {
+        const channelButton = document.createElement("button");
+        channelButton.classList.add("channel-button");
+        channelButton.textContent = channelName;
+        channelList.appendChild(channelButton);
+
+        channelButton.addEventListener("click", () => {
+            alert(`Has hecho clic en el canal: ${channelName}`);
+       
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
 
 //MENSAJES
 const chatDisplay = document.getElementById("chat-display");
